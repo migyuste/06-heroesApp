2f12ce751ee81bfdebb6b69f479f08d35c2e8252
Miguel Yuste <yusaguilar@gmail.com>
2023-04-17 00:39:08 +0200

    hero page

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from 'src/environments/environments';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class HeroesService {
-    private baseUrl: string = environments.baseUrl;
-    constructor(private http: HttpClient) { }
-    getHeroes(): Observable<Hero[]> {
-        return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
-    }
-    
+  private baseUrl: string = environments.baseUrl;
+  constructor(private http: HttpClient) {}
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
+  }
+  getHeroById(id: string): Observable<Hero | undefined> {
+    return this.http
+      .get<Hero>(`${this.baseUrl}/heroes/${id}`)
+      .pipe(catchError((error) => of(undefined)));
+  }
 }
