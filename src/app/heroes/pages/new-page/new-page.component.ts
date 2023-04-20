cb41e8ea4c6cf27a473aaa7f7f4ec9d34d1d4261
Miguel Yuste <yusaguilar@gmail.com>
2023-04-20 22:06:08 +0200

    199. Agregar Héroe - HTML

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NewPageComponent {
-
+  public publishers = [
+    { id: 'DC Comics', value: 'DC - Comics' },
+    { id: 'Marvel Comics', value: 'Marvel - Comics' },
+  ];
 }
